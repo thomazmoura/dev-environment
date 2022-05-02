ed8fc4b06109d58f3825c399e801d169deff211b
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-02 03:59:47 +0000

    Update git-setup.sh

--- Kernel/shell/git-setup.sh
+++ Kernel/shell/git-setup.sh
@@ -9,3 +9,20 @@ git config --global push.default simple
 git config --global core.quotepath off
 git config --global fetch.prune true
 git config --global core.excludesFile $HOME/.shell/gitignore
+
+#  Delta configuration
+git config --global core.pager 'delta'
+git config --global interactive.diffFilter 'delta --color-only --features=interactive'
+git config --global delta.navigate 'true'
+git config --global merge.conflictstyle 'diff3'
+git config --global diff.colorMoved 'default'
+git config --global delta.features 'decorations'
+git config --global delta.interactive.keep-plus-minus-markers 'false'
+git config --global delta.decorations.commit-decoration-style 'blue ol'
+git config --global delta.decorations.commit-style 'raw'
+git config --global delta.decorations.file-style 'omit'
+git config --global delta.decorations.hunk-header-decoration-style 'blue box'
+git config --global delta.decorations.hunk-header-file-style 'red'
+git config --global delta.decorations.hunk-header-line-number-style '"#067a00"'
+git config --global delta.decorations.hunk-header-style 'file line-number syntax'
+
