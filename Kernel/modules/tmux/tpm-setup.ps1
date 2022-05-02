4804ae5042d8021e861a43f5257082a9b3f515bc
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-02 05:20:57 +0000

    Update tmux plugin installation to use the aproppriate version

--- Kernel/modules/tmux/tpm-setup.ps1
+++ Kernel/modules/tmux/tpm-setup.ps1
@@ -1,12 +1,6 @@
 Write-Output "`n->> Installing tpm"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm --depth 1
-
-# start a server but don't attach to it
-tmux start-server
-# create a new session but don't attach to it either
-tmux new-session -d
+#
 # install the plugins
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
-# killing the server is not required, I guess
-tmux kill-server
+~/.tmux/plugins/tpm/bin/install_plugins
 
