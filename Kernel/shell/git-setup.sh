f905a863c141441265cc0fce5418bc2d847cd755
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-06 12:33:04 +0000

    Fix git-setup.sh on startup

--- Kernel/shell/git-setup.sh
+++ Kernel/shell/git-setup.sh
@@ -1,3 +1,3 @@
-Write-Output "Setting git config for $env:GIT_USERNAME"
+echo "Setting git config for $env:GIT_USERNAME"
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
