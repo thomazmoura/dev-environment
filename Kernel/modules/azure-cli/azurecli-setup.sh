3b339a257d30a5f58383135e0868c11310198e5b
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-07 05:00:59 +0000

    Remove powershell version and keep azurecli-setup.sh instead

--- Kernel/modules/azure-cli/azurecli-setup.sh
+++ Kernel/modules/azure-cli/azurecli-setup.sh
@@ -8,7 +8,7 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc |
 
 echo "Adding the Azure CLI software repository"
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ AZ_REPO main" |
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     tee /etc/apt/sources.list.d/azure-cli.list
 
 echo "Updating apt-get and installing azure-cli"
