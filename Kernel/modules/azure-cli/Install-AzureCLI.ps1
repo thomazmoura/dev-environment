714e8f068eee15765de6f949d1e6d3091d41783c
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-07 04:25:32 +0000

    Attempt at fixing azure-cli installation (failed for not having root)

--- Kernel/modules/azure-cli/Install-AzureCLI.ps1
+++ Kernel/modules/azure-cli/Install-AzureCLI.ps1
@@ -4,12 +4,12 @@ apt-get update & apt-get install ca-certificates curl apt-transport-https lsb-re
 Write-Output "Downloading Microsoft signing key"
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+    tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 
 Write-Output "Adding the Azure CLI software repository"
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+    tee /etc/apt/sources.list.d/azure-cli.list
 
 Write-Output "Updating apt-get and installing azure-cli"
 apt-get update
