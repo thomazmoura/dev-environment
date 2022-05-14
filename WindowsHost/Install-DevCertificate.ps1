87af74b9e2f044ab80b22714ac615fe83d2f883a
Thomaz Moura <padilha.thomaz@gmail.com>
2022-05-14 15:04:10 +0000

    Add script to install dev-certificate on Windows using the shared volume

---
+++ WindowsHost/Install-DevCertificate.ps1
@@ -0,0 +1,20 @@
+param(
+  [String] $PathToSharedCertificate = "$HOME/.shared/aspnet-localhost.pfx"
+  [SecureString] $Password
+)
+
+if( !(Test-Path $PathToSharedCertificate) ) {
+  Write-Warning "No certificate was found on $PathToSharedCertificate. Cancelling"
+  return;
+}
+
+Write-Information "Importing the certificate"
+& dotnet dev-certs https --clean --import $PathToSharedCertificate -p $Password
+
+Write-Information "Trusting the certificate"
+& dotnet dev-certs https --check --trust
+
+Write-Information "Saving the default path and password for the certificate"
+[System.Environment]::SetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path", $PathToSharedCertificate, "User")
+[System.Environment]::SetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password", $Password, "User")
+
