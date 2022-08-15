f3ee90bcc9e574c2a6e62a7dcbf0fe11cc27186e
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-15 00:38:28 -0300

    Add full path to az call

--- modules/azure-cli/azurecli-setup.sh
+++ modules/azure-cli/azurecli-setup.sh
@@ -1,7 +1,6 @@
 echo "->> Installing Azure CLI from pip" 
-apt install python3 -y
 pip install azure-cli
 
 echo "->> Adding azure devops extensions"
-az extension add --name azure-devops
+/usr/bin/az extension add --name azure-devops
 
