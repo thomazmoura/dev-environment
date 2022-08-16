c3ae4e20174de4e65e8db150b00b75d61fce05bd
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-16 17:36:39 -0300

    Install ps on debian so tmux navigator can work

--- modules/tmux/tpm-setup.sh
+++ modules/tmux/tpm-setup.sh
@@ -4,3 +4,5 @@ git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm --depth 1
 # install the plugins
 ~/.tmux/plugins/tpm/bin/install_plugins
 
+apt update && apt install procps && rm -rf /var/lib/apt/lists/*
+
