d5dfda0a81c5a776db04b771fa7e4aebb3cc95d2
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-16 14:42:48 +0000

    Revert "Attempt at rooling back csharp-ls version"
    This reverts commit c40cab96cfdf05d7d67ee9157c6db86df848723f.

--- modules/dotnet-tools/dotnettools-setup.ps1
+++ modules/dotnet-tools/dotnettools-setup.ps1
@@ -1,5 +1,5 @@
 Write-Output "`n->> Installing global .net tools"
-& dotnet tool install --global --version 0.5.2 csharp-ls
+& dotnet tool install --global csharp-ls
 & dotnet tool install --global dotnet-ef
 & dotnet tool install --global dotnet-outdated-tool
 
