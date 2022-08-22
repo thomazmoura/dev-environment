ada684b07e58389c3bd65b586bc5c3a4a7c6047f
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-22 13:31:55 -0300

    Fix typo

--- modules/rust/Install-Rust.ps1
+++ modules/rust/Install-Rust.ps1
@@ -1,7 +1,7 @@
 . "$HOME/.modules/powershell/Check-Failure.ps1"
 
 Write-Information "`n->> Installing Rust (with RustUp)"
-& curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
+& curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 Write-Information "`n->> Installing Cargo modules"
 & cargo install cargo-watch
