e9375e80f7638ab8d36acff7ee40c9cbfca93072
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-22 13:41:25 -0300

    Update path before calling cargo

--- modules/rust/Install-Rust.ps1
+++ modules/rust/Install-Rust.ps1
@@ -3,6 +3,10 @@
 Write-Information "`n->> Installing Rust (with RustUp)"
 & curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+Write-Information "`n->> Updating path to include cargo"
+$UpdatedPath = "$env:PATH:$HOME/.cargo/bin"
+[System.Environment]::SetEnvironmentVariable("PATH", $UpdatedPath, "Process");
+
 Write-Information "`n->> Installing Cargo modules"
 & cargo install cargo-watch
 
