d0995073f03a6c64ce9e717788e5e6b315dffa85
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-28 11:14:46 -0300

    Fix PowerShell on code on .sh file

--- modules/qmk/qmk_setup.sh
+++ modules/qmk/qmk_setup.sh
@@ -1,6 +1,6 @@
 python3 -m pip install --user -r /home/developer/.modules/qmk/requirements.txt
 python3 -m pip install --user qmk 
 
-$env:PATH="$env:PATH`:/home/developer/.local/bin"
+export PATH="/home/developer/.local/bin:$PATH"
 qmk setup
 
