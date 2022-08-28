b6f0c8e5451c5cbc00b12acafcb3668e9f4aa73e
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-27 22:41:29 -0300

    Clear bootload installation files before next snapshot

--- modules/qmk/qmk_install.sh
+++ modules/qmk/qmk_install.sh
@@ -9,9 +9,10 @@ _qmk_install_bootloadhid() {
         wget https://www.obdev.at/downloads/vusb/bootloadHID.2012-12-08.tar.gz -O - | tar -xz -C /tmp
         pushd /tmp/bootloadHID.2012-12-08/commandline/ > /dev/null
         if make; then
-            cp bootloadHID /usr/local/bin
+            mv bootloadHID /usr/local/bin
         fi
         popd > /dev/null
+        rm -rf /tmp/bootloadHID.2012-12-08/
     fi
 }
 
