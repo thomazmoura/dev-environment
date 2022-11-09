9b0ca2e75cd89fe04c96b04ad41679662b4168ce
Thomaz Moura <padilha.thomaz@gmail.com>
2022-11-09 11:19:50 -0300

    Add parameter to start docker with specific command

--- scripts/Start-Docker.ps1
+++ scripts/Start-Docker.ps1
@@ -1,6 +1,10 @@
+param(
+  [String]$Command='if( tmux ls 2> $null ) { tmuxa } else { vtmux }'
+)
+
 Write-Information "\n->> Starting dev-env container"
 sudo docker start dev-env
 
 Write-Information "\n->> Attaching to dev-env container"
-sudo docker container exec -it dev-env pwsh -c 'if( tmux ls 2> $null ) { tmuxa } else { vtmux }'
+sudo docker container exec -it dev-env pwsh -c $Command
 
