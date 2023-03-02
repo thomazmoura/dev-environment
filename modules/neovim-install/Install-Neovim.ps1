1914d23113812119f77cc9113df292d0b9df233e
Thomaz Moura <padilha.thomaz@gmail.com>
2023-03-02 19:53:59 -0300

    Fixing access issue (copying to root)

--- modules/neovim-install/Install-Neovim.ps1
+++ modules/neovim-install/Install-Neovim.ps1
@@ -1,15 +1,32 @@
 . "$HOME/.modules/powershell/Check-Failure.ps1"
 
 Write-Output ">>> Installing NeoVim"
+Set-Location $HOME
 New-Item -Type Directory -Force "$HOME/neovim/"
+
+Write-Output "=>> Ensuring the ~/.local/bin directory exists"
 New-Item -Type Directory -Force "$HOME/.local/bin/"
+
+Write-Output "=>> Downloading Neovim's realease tar package"
 Invoke-WebRequest https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -OutFile nvim-linux64.tar.gz
+
+Write-Output "=>> Extracting Neovim package"
 & tar -xzf nvim-linnux64.tar.fz
+
+Write-Output "=>> Moving Neovim files to expected destination"
 Move-Item nvim-linnux64/* "$HOME/neovim/"
+
+Write-Output "=>> Making nvim (Neovim) executable"
+& chmod +x $HOME/neovim/bin/nvim
+
+Write-Output "=>> Creating symbolic links"
 New-Item -Type SymbolicLink -Path "$HOME/.local/bin/nvim" -Target "$HOME/neovim/bin/nvim"
 New-Item -Type SymbolicLink -Path "$HOME/.local/bin/vim" -Target "$HOME/neovim/bin/nvim"
 New-Item -Type SymbolicLink -Path "$HOME/.local/bin/vi" -Target "$HOME/neovim/bin/nvim"
-& chmod +x $HOME/neovim/bin/nvim
+
+Write-Output "=>> Cleaning up"
+Remove-Item -Recure -Force nvim-linux64.tar.gz
+Remove-Item -Recure -Force nvim-linux64
 
 Throw-ExceptionOnNativeFailure
 
