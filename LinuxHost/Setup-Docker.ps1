b12128e2976c8bcbcc9720d98f6f87cba2fcb258
Thomaz Moura <padilha.thomaz@gmail.com>
2023-04-30 22:40:43 -0300

    Add code to LinuxHost/Setup-Docker.ps1 fix volume folders permissions

--- LinuxHost/Setup-Docker.ps1
+++ LinuxHost/Setup-Docker.ps1
@@ -25,6 +25,11 @@ sudo docker volume create storage *> /dev/null
 Write-Information "`n->> Creating the network (if it does not exist)"
 sudo docker network create dev-environment-network *> /dev/null
 
+Write-Information "`n->> Fixing the volume folders permissions"
+sudo docker container run --user root:root -v "$HOME/.shared:/home/developer/.shared" --env-file "$HOME/.docker-variables" -it --rm --name dev-env-fix thomazmoura/dev-environment$ContainerTag /bin/bash -c "chown -R developer:developer /home/developer/.shared"
+sudo docker container run --user root:root -v storage:/home/developer/.storage --env-file "$HOME/.docker-variables" -it --rm --name dev-env-fix thomazmoura/dev-environment$ContainerTag /bin/bash -c "chown -R developer:developer /home/developer/.storage"
+sudo docker container run --user root:root --env-file "$HOME/.docker-variables" -it --rm --name dev-env-fix thomazmoura/dev-environment$ContainerTag /bin/bash -c "chown -R developer:developer /home/developer/.hosts"
+
 Write-Information "`n->> Creating the $ContainerName container"
 sudo docker container run -v code:/home/developer/code -v storage:/home/developer/.storage -v "$HOME/.shared:/home/developer/.shared" -p 4200:4200 -p 5000:5000 -p 5001:5001 -p 5500:5500 -p 5501:5501 --env-file "$HOME/.docker-variables" -it -d --network dev-environment-network --name $ContainerName thomazmoura/dev-environment$ContainerTag
 
