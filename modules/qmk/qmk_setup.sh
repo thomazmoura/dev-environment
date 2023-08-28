008956f3981b503ab4f73bf82e3bf5e2ddcbe713
Thomaz Moura <thomaz.moura@autotrac.com.br>
2023-08-28 01:22:13 -0300

    New attempt at fixing python dependencies for QMK with pipx

--- modules/qmk/qmk_setup.sh
+++ modules/qmk/qmk_setup.sh
@@ -1,5 +1,5 @@
-python3 -m pipx install --user -r /home/developer/.modules/qmk/requirements.txt
-python3 -m pipx install --user qmk 
+python3 -m pipx install -r /home/developer/.modules/qmk/requirements.txt
+python3 -m pipx install qmk 
 
 export PATH="/home/developer/.local/bin:$PATH"
 qmk setup
