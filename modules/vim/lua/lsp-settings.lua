1c773b37c2724e4969e19952960619d342a110a4
Thomaz Moura <padilha.thomaz@gmail.com>
2024-05-21 11:33:57 -0300

    Disable tsserver's rename in favor of angular-ls

--- modules/vim/lua/lsp-settings.lua
+++ modules/vim/lua/lsp-settings.lua
@@ -254,16 +254,25 @@ lspconfig.jsonls.setup {
   flags = lsp_flags,
 }
 
--- TypeScript LS settings
+-- Custom on_attach to disable tsserver's rename so that angular-ls rename is always used instead
+local function disable_tsserver_rename_on_attach(client, bufnr)
+  if client.name == 'tsserver' then
+    client.server_capabilities.renameProvider = false
+  end
+end
+
+--TypeScript LS settings
 lspconfig.tsserver.setup {
   capabilities = capabilities,
   flags = lsp_flags,
+  on_attach = disable_tsserver_rename_on_attach
 }
 
 -- Angular LS settings
 lspconfig.angularls.setup {
   capabilities = capabilities,
   flags = lsp_flags,
+  on_attach = disable_tsserver_rename_on_attach
 }
 
 -- YAML LS settings
