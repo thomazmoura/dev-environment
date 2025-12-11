e3166dba001f3c3f3c3884df49615d42287eea65
Thomaz Moura <padilha.thomaz@gmail.com>
2025-12-11 19:08:07 -0300

    Add flameshot installation (for gnome + wayland)

---
+++ LinuxDevEnv/flameshot-install.sh
@@ -0,0 +1,17 @@
+# This script is intended to install and enable flameshot as the default gui screen capture on gnome + wayland when pressing print screen
+
+sudo apt install flameshot xdg-desktop-portal xdg-desktop-portal-gnome
+
+# Register custom shortcut path
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+  "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Configure the shortcut
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+  name "Flameshot"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+  command "/home/thomaz.moura/.local/bin/flameshot-gui.sh"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ \
+  binding "Print"
