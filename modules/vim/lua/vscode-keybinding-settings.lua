519158e3f0d7458a65eda74c8f88c3423cdaff84
Thomaz Moura <padilha.thomaz@gmail.com>
2025-12-18 19:58:21 -0300

    Add <leader>R to the VS Code database mappings

--- modules/vim/lua/vscode-keybinding-settings.lua
+++ modules/vim/lua/vscode-keybinding-settings.lua
@@ -9,6 +9,15 @@ local function execute_sql_command(db_type)
   end
 end
 
+-- Helper to execute full SQL query based on db type
+local function execute_sql_run_query(db_type)
+  if db_type == 'mssql' then
+    vim.fn.VSCodeNotify('mssql.runQuery')
+  else
+    vim.fn.VSCodeNotify('pgsql.runQuery')
+  end
+end
+
 -- SQL Query Runner - per-buffer database type selection
 local function run_sql_query()
   local db_type = vim.b.sql_db_type
@@ -35,6 +44,31 @@ local function run_sql_query()
   )
 end
 
+-- Run full SQL query - per-buffer database type selection
+local function run_full_sql_query()
+  local db_type = vim.b.sql_db_type
+
+  if db_type then
+    execute_sql_run_query(db_type)
+    return
+  end
+
+  vim.ui.select(
+    { 'SQL Server', 'PostgreSQL' },
+    { prompt = 'Select database type:' },
+    function(choice)
+      if choice == 'SQL Server' then
+        vim.b.sql_db_type = 'mssql'
+      elseif choice == 'PostgreSQL' then
+        vim.b.sql_db_type = 'pgsql'
+      else
+        return
+      end
+      execute_sql_run_query(vim.b.sql_db_type)
+    end
+  )
+end
+
 -- Reset database type for current buffer
 local function reset_sql_db_type()
   vim.b.sql_db_type = nil
@@ -49,7 +83,8 @@ vim.api.nvim_create_autocmd('FileType', {
   group = 'VSCodeSqlFiles',
   pattern = 'sql',
   callback = function()
-    vim.keymap.set('n', '<leader>r', run_sql_query, default_buffer_options)
+    vim.keymap.set({'n', 'v'}, '<leader>r', run_sql_query, default_buffer_options)
+    vim.keymap.set({'n', 'v'}, '<leader>R', run_full_sql_query, default_buffer_options)
     vim.keymap.set('n', '<leader>mc', reset_sql_db_type, default_buffer_options)
   end
 })
